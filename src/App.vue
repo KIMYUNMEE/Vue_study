57b46859c99c58145ed3744b34bef0d1253f2e58
KIMYUNMEE <jayoo8665@naver.com>
2022-08-02 22:49:42 +0900

    :memo: Update 데이터바인딩
    데이터바인딩

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <div>
+    <h4>XX 원룸</h4>
+    <p>{{price1}} 만원</p>
+  </div>
+  <div>
+    <h4>XX 원룸</h4>
+    <p>XX 만원</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name : 'App',
+  data(){
+    return {
+      price1 : 60
+    }
   }
 }
 </script>
